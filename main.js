56230051ce64e8f2bd8129b2428e63c541f855c4
Nelson Agamata <nelson.s.agamata@gmail.com>
2020-04-22 22:32:55 +0800

    Finish a browser version of palindrome detector

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("nelsonagamata-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palidromeTester(event) {
+	event.preventDefault();
+	
+	let phrase = new Phrase(event.target.phrase.value);
+	let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-	alert(`"${phrase.content}" is a palindrome!`);
-} else {
-	alert(`"${phrase.content}" is NOT a palindrome!`);
+	if (phrase.palindrome()) {
+		palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+	} else {
+		palindromeResult.innerHTML = `"${phrase.content}" is NOT a palindrome!`;
+	}
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+	let button = document.querySelector("#palindromeTester");
+	button.addEventListener("submit", function(){
+		palidromeTester(event);
+	});
+});
